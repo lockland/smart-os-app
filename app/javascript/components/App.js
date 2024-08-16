9a8bf569ced34355c7a864f04812b7da48119dc5
Sidney Souza <sidneysase@gmail.com>
2024-08-16 11:53:26 -0300

    add skeleton app

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,11 +1,41 @@
 import React from "react"
-import { Switch } from "@mantine/core"
 import "./App.css"
+import { AppShell, Burger } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import NavbarSimple from "./NavbarSimple"
+import Main from "./Main";
 
 function App() {
-    return (
-        <Switch defaultChecked label="This is my app component with mantine" />
-    )
+  const [opened, { toggle }] = useDisclosure();
+
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: 'sm',
+        collapsed: { mobile: !opened },
+      }}
+      padding="md"
+    >
+      <AppShell.Header bg={"var(--primary-background-color)"}>
+        <Burger
+          opened={opened}
+          onClick={toggle}
+          hiddenFrom="sm"
+          size="xl"
+        />
+      </AppShell.Header>
+
+      <AppShell.Navbar p="md">
+        <NavbarSimple />
+      </AppShell.Navbar>
+
+      <AppShell.Main>
+        <Main />
+      </AppShell.Main>
+    </AppShell>
+  );
 }
 
 export default App
---
+++ app/javascript/components/Main.js
@@ -0,0 +1,8 @@
+import { Switch } from "@mantine/core";
+import React from "react";
+
+export default function Main() {
+  return (
+    <Switch defaultChecked label="Olá Everton!! Seu app está no forno" />
+  )
+}
